3780d264ad019358aeda2704cc0f32dda27232ec
Lee Wan Sze <leewansze27@gmail.com>
2021-11-06 15:18:55 +0000

    feat: scaled raw data to display prices between 0 - 100

--- cryptocurrencyAnalysis.py
+++ cryptocurrencyAnalysis.py
@@ -12,8 +12,8 @@ import matplotlib.dates as mdates
 
 # Save crypto data into a csv file
 
-start_date = '2021-10-01'
-end_date = '2021-11-03'
+start_date = '2021-01-01'
+end_date = '2021-11-06'
 
 cryptos = ['BTC-USD', 'ETH-USD', 'BNB-USD', 'SHIB-USD'] 
 
@@ -29,14 +29,7 @@ eth_df = pd.read_csv(filepaths[1])
 bnb_df = pd.read_csv(filepaths[2])
 shib_df = pd.read_csv(filepaths[3])
 
-df = pd.DataFrame({'Date' : btc_df['Date'] , 'BTC': btc_df['Close'], 'ETH': eth_df['Close'], 'BNB' : bnb_df['Close'], 'SHIB' : shib_df['Close']})
-
-
-print(df)
-df.plot(x = 'Date' ,figsize = (15,8))
-plt.grid()
-plt.ylabel("Price in USD")
-plt.show()
+df = pd.DataFrame({'BTC': btc_df['Close'], 'ETH': eth_df['Close'], 'BNB' : bnb_df['Close'], 'SHIB' : shib_df['Close']})
 
 # scale the data
 from sklearn import preprocessing
@@ -44,12 +37,15 @@ min_max_scaler = preprocessing.MinMaxScaler(feature_range=(0,100))
 scaled = min_max_scaler.fit_transform(df)
 print(scaled)
 
-# convert scaled numpy array into dataframe
+# convert scaled numpy array into dataframe and plot it
 df_scaled = pd.DataFrame(scaled, columns = df.columns)
+df_scaled['Date'] = btc_df['Date']
 
 plt.style.use('fivethirtyeight')
-df.plot(x = 'Date' ,figsize = (15,8))
-plt.title("Crypto scaled graph")
+df_scaled.plot(x = 'Date' ,figsize = (15,8), fontsize=11, ylim = [0,100])
+plt.xlabel("Days")
+plt.ylabel("Scaled Crypto Price ($)", fontdict={'fontsize':11})
+plt.title("Crypto scaled graph", fontdict={'fontsize':11})
 plt.show()
 
 
